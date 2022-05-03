63238232b7e59dcc7ec61587f646fa35ec653f0e
Conor Pierce <cpierce@cpiercedevelopment.com>
2022-05-03 13:50:16 -0700

    Update hx711.py
    Change sps to board_freq to avoid confusion with sample_interval.

--- klippy/extras/hx711.py
+++ klippy/extras/hx711.py
@@ -72,7 +72,7 @@ class PrinterHx711:
         self.sck_pin = sck_pin_params['pin']
         self.mcu = dout_pin_params['chip']
         self.gain = config.getchoice('gain', {32: 2, 64: 3, 128: 1}, default=64)
-        self.sps = config.getchoice('sps',{10: 10, 80: 80}, default=10)
+        self.sps = config.getchoice('board_freq',{10: 10, 80: 80}, default=10)
         self.sample_interval = config.getint('sample_interval', default=1)
         self.comm_delay = config.getint('comm_delay', default=1)
         self.config = config
